9f0a4282d757b4bef39cb32b53d92d945acf4f6b
Ioannis (Giannis) Siriopoulos <57215843+ivsyriopoulos@users.noreply.github.com>
2025-02-10 19:29:07 +0100

    Changed image name in compose - removed some unessecary lines in student_service

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -4,32 +4,19 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from bson import ObjectId
 
-try:
-    uri = "mongodb://mongo:27017/"
-    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
-    database = client["students_db"]
-    students_collection = database["students_collection"]
-    client.server_info()  # Force connection test
-except ServerSelectionTimeoutError:
-    print("MongoDB server is not available. Check connection settings.")
+uri = "mongodb://mongo:27017/"
+client = MongoClient(uri)
+database = client["students_db"]
+students_collection = database["students_collection"]
 
 
 def add(student=None):
-    print(f'searching for student {student}')
     query = {"first_name": student.first_name, "last_name": student.last_name}
+    res = students_collection.find_one(query)
+    if res:
+        return 'already exists', 409
 
-    print("query database")
-    dbnames = client.list_database_names()
-    if 'students_db' in dbnames:
-        list_of_collections = database.list_collection_names()
-        if "students_db" in list_of_collections:
-            res = students_collection.find_one(query)
-            if res:
-                return 'already exists', 409
-
-    print(f'No student found, inserting')
     result = students_collection.insert_one(student.to_dict())
-    print(f'inserted {result.inserted_id}')
     student.student_id = str(result.inserted_id)
     return student.student_id
 
